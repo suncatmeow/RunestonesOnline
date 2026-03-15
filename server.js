eb1df13eb5c2f37ec981822f2fe3d0e72bd42cab
suncatmeow <suncat.data@gmail.com>
2026-03-14 22:13:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2407,7 +2407,7 @@ const broadcastSuncatMessage = (fullResponse, options = {}) => {
         const sendBorder = () => {
             const borderPayload = {
                 sender: "",
-                text: " ",                
+                text: "✧ ooooooooooooooooooooooooooooooooooooooooooooooo ✧",                
                 color: "#555555"
             };
             if (options.targetId) {
