952601bfaaf5fa7e7711306f4d66e87b1cb5a140
suncatmeow <suncat.data@gmail.com>
2026-05-04 07:38:34 -0500

    Increase NPC spawn rates and adjust loops

--- server.js
+++ server.js
@@ -4002,7 +4002,7 @@
                             });
                             
                             // 3. Bastion Guards (Defenders - Will actively hunt invaders!)
-                            for (let i = 0; i < 1; i++) {
+                            for (let i = 0; i < 3; i++) {
                                 let defID = friendlyMinions[i % friendlyMinions.length] || 64; 
                                 mapNPCs.push({
                                     type: CARD_MANIFEST_DB[defID].sprite || defID, 
@@ -4015,7 +4015,7 @@
                             }
 
                             // 4. Civilians (Friendlies - Will run or be killed if invaded!)
-                            for (let i = 0; i < 1; i++) {
+                            for (let i = 0; i < 6; i++) {
                                 let civID = friendlyMinions[Math.floor(Math.random() * friendlyMinions.length)] || 32; 
                                 mapNPCs.push({
                                     type: CARD_MANIFEST_DB[civID].sprite || civID, 
@@ -4091,7 +4091,7 @@
                             let shuffledFloors = [...mapData.validFloors].sort(() => 0.5 - Math.random());
                             
                             // --- FIX: Halved the spawn rate! (1 NPC per 150 tiles) ---
-                            let totalWanderers = Math.floor(shuffledFloors.length / 150); 
+                            let totalWanderers = Math.floor(shuffledFloors.length / 75); 
 
                             for (let i = 0; i < totalWanderers; i++) {
                                 let tile = shuffledFloors[i];
